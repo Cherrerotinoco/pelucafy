c27fc6d9bbce01bb92d9f6958c15f40dd0d513a0
paulMolano <paulmolanoo@.com>
2022-01-27 10:38:16 +0100

    mensaje error signup

--- packages/web/src/pages/SignUp/SignUp.js
+++ packages/web/src/pages/SignUp/SignUp.js
@@ -99,7 +99,7 @@ function SignUp() {
               Sign Up
             </button>
           </form>
-          {signUpError && <section className="mt-4">{signUpError}</section>}
+          {signUpError && <section className="mt-4">{signUpError.payload}</section>}
           <section className="mt-4">
             <hr className="mt-1 mb-4" />
             <Link

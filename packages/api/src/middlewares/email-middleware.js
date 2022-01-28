6ffd04c0d9f177da1e361466731f8e9f36f53a0d
Cherrerotinoco <c.herrerotinoco@gmail.com>
2022-01-28 12:22:24 +0100

    fix: emailChangeMiddleware

--- packages/api/src/middlewares/email-middleware.js
+++ packages/api/src/middlewares/email-middleware.js
@@ -1,23 +1,22 @@
 const { auth } = require("../services");
 
 async function emailChangeMiddelware(req, res, next) {
-    
-  const newEmail = req.body.email
-  const uid = req.user.uid
-  
-  if (newEmail !== req.body.email) next()
+  const newEmail = req.body.email;
+  const uid = req.user.uid;
+
+  if (newEmail !== req.user.email) next();
 
   try {
-    await auth.updateUser(uid, {email: newEmail})
-    next()  
+    await auth.updateUser(uid, { email: newEmail });
+    next();
   } catch (error) {
     res.status(500).send({
       data: null,
       error: error.message,
     });
   }
-} 
+}
 
 module.exports = {
-  emailChangeMiddelware
-}
+  emailChangeMiddelware,
+};

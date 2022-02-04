0cc7d980b63e8407af59f8998bad411e3fcd6760
ParisArcos <84467828+ParisArcos@users.noreply.github.com>
2022-02-04 13:16:40 +0000

    fix: particles

--- packages/web/src/components/elements/Title.js
+++ packages/web/src/components/elements/Title.js
@@ -3,9 +3,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const Title = ({ weight, align, children }) => {
-  console.log();
   const style = `m-4 text-${weight}xl md:text-${weight}xl text-white  font-bold leading-tight text-${align} md:text-${align}`;
-  console.log(style);
   return (
     <>
       <h1 className={style}>{children}</h1>
